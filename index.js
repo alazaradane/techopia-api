da0195b484de2b84c19160357e299954ed96ca24
alazaradane <alazargithub369@gmail.com>
2024-06-24 17:04:40 +0300

    adding route setup in index

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
+
+app.use('/api/auth', authRoutes)
+app.use('/api/projects', projectRoutes)
+app.use('/api/blogs', blogRoutes)
+app.use('/api/events', eventRoutes)
+app.use('/api/newsletter', newsletterRoutes)
+
+
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
         console.log(`Listening port ${PORT}`)
